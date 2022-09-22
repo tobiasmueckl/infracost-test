d47939df4997ef57e5b2866e87c0a2adfa48ef67
tobiasmueckl <113180465+tobiasmueckl@users.noreply.github.com>
2022-09-22 09:48:22 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ terraform {
 }
 
 resource "aws_db_instance" "db" {
-    allocated_storage    = 20
+    allocated_storage    = 2000
     engine               = "mysql8"
     engine_version       = "8.0.28"
     instance_class       = "db.t4g.micro"
